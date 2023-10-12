092ed8eafd608784c3b2d90800a17cde4e37cd19
Shashikant Yadav <103162888+shashi310@users.noreply.github.com>
2023-10-12 14:12:12 +0530

    backend landing Page Fix

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
 const app = express();
-
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to the backend of Skillvantage Learning Platform");
+});
 app.use(cors())
 
 app.use(express.json());
